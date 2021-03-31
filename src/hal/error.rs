1d1048623bfae3b64bd0b144b9ebf7aa4009137b
ryan <ryan@kurte.nz>
2021-03-31 14:07:14 +1300

    add functions to check hal error types

--- src/hal/error.rs
+++ src/hal/error.rs
@@ -15,6 +15,28 @@ pub enum HalError {
     Sysfs(linux_embedded_hal::sysfs_gpio::Error),
 }
 
+impl HalError {
+    /// Check whether the HalError contains an underlying error type
+    pub fn is_inner(&self) -> bool {
+        use HalError::*;
+
+        match self {
+            InvalidConfig | InvalidSpiMode | NoPin => false,
+            _ => true,
+        }
+    }
+
+    /// Check whether the HalError signals no pin is available
+    pub fn is_no_pin(&self) -> bool {
+        use HalError::*;
+
+        match self {
+            NoPin => true,
+            _ => false,
+        }
+    }
+}
+
 #[cfg(feature = "hal-cp2130")]
 impl From<driver_cp2130::Error> for HalError {
     fn from(e: driver_cp2130::Error) -> Self {
